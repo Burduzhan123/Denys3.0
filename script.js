21246e81d5a2464f6d44ae0dc5eee673e5fda71d
Denys <denisburduzan@gmail.com>
2023-12-05 17:00:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const condition = 15
+
+if (condition === 9) {
+    console.log('fizz'); 
+} else if (condition === 10) {
+    console.log('Buzz');
+}else if (condition === 15){
+    console.log('FizzBuzz');
+}
